98262d107ff0c32f8d20b0ef4cc44d4a035c6a04
Unbound3d <68504943+Unbound3d@users.noreply.github.com>
2023-01-25 23:50:22 +0300

    User profile Update feature
    Added a feature that allows users to update their profile info and picture

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(UserCreationForm):  #This class creates the User registration form
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+
+class UserUpdateForm(forms.ModelForm):   #This class creates the User details update form to update a user's email and password
+     email = forms.EmailField()
+     
+     class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm): #This class will allow users to update their profile picture
+    class Meta:
+        model = Profile
+        fields = ['image']
+
